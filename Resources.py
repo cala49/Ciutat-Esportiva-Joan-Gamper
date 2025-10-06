23388fda1da16d422ea4a07d4463f1872e328ffe
CarlosCala <carlosmarichalcala@gmail.com>
2025-10-06 03:02:33 -0400

    Implementation of Sports Equipament

--- Resources.py
+++ Resources.py
@@ -1,28 +1,33 @@
+
 class Resource:
-    def __init__(self, name, inStack = 0):        
-        self.name = name
+    def __init__(self, type: str, index: int, name: str, inStack: int):        
+        self.type = type
+        self.index = index
+        self.name = name       
         self.inStack = inStack        
     def __repr__(self):
-        return f"Resource({self.name}, {self.inStack} in stack.)"
+        return f"Resource({self.type}, id: {self.index}, {self.name}, {self.inStack} in stack.)"
 
 
 class Ball(Resource):
-    def __init__(self, name, brand, inStack=0):
-        super().__init__(name, inStack)  # Llama a los atributos del padre
+    def __init__(self, index: int, name: str, brand: str, inStack: int):
+        super().__init__(self, "Sports Equipament", index, name, inStack)  # Llama a los atributos del padre
         self.brand = brand  # Nuevo atributo
            
     def __repr__(self):
-       return f"Ball({self.name}, {self.brand} brand, {self.inStack} in stack.)"
+       return f"Ball({self.type}, id: {self.index}, {self.name}, {self.brand} brand, {self.inStack} in stack.)"
 
 
 class Cards(Resource):
-    def __init__(self, name, inStack=0):
-        super().__init__(name, inStack)  # Llama a los atributos del padre
+    def __init__(self, index: int, name: str, inStack: int):
+        super().__init__("Sports Equipament", index, name, inStack)  # Llama a los atributos del padre        
+    def __repr__(self):  
+        return f"Cards({self.type}, id: {self.index}, {self.name}, {self.inStack} in stack.)"
+
+
+class Kit(Resource):
+    def __init__(self, index: int, name: str, color: str, inStack: int):
+        super().__init__("Sports Equipament", index, name, inStack)   # Llama a los atributos del padre   
+        self.color = color  # Nuevo atributo     
     def __repr__(self):
-        return f"Cards({self.name}, {self.inStack} in stack.)"
-
-Mercury = Ball("Mercury", "Adidas", 5)
-print(Mercury)
-
-yellow = Cards("Yellow", 2)
-print(yellow)
+        return f"Kit({self.type}, id: {self.index}, {self.name}, {self.color} color, {self.inStack} in stack.)"
